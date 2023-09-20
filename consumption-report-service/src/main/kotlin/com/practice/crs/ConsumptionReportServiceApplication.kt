a367a14fd89c5b8e487bf1650d2e9ab83e2a4b20
Vaibhav Desai <vaibhavdesai16@gmail.com>
2023-09-20 16:11:53 +0530

    initial commit | basic docker setup

---
+++ consumption-report-service/src/main/kotlin/com/practice/crs/ConsumptionReportServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.crs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConsumptionReportServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConsumptionReportServiceApplication>(*args)
+}
